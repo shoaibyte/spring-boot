bf170e120d64e03d2297016c5001762607ccc70c
Shoaib <shoaib@softwrd.ai>
2025-09-14 22:00:22 +0600

    initial commit

---
+++ src/test/java/com/shoaib/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoaib.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
